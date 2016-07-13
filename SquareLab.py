e3ca98f861932df0353efcc19900d01c70a932f0
ELojanoASC <ErickJLojano@gmail.com>
2016-07-13 11:25:44 -0400

    Square Lab

--- SquareLab.py
+++ SquareLab.py
@@ -1,11 +1,18 @@
 # Square Lab
-from Myro import * #ImportAllFunctions
+from Myro import * #ImportAllFunction
 init("sim") #StartSimulator
 
-x = 1 #Speed
-y = 1 #seconds
+x = 2 #Speed
+y = 2 #seconds
 
 d = 90 #Degrees
 z = "deg" #Measurement
 
-forward(x,y),turnBy(d,z),forward(x,y),turnBy(d,z),forward(x,y),turnBy(d,z),forward(x,y)
+i = 0 #StartCounter
+penDown() # Put pen down
+
+while i < 4:
+    forward(x,y)
+    turnBy(d,z)
+    i = i + 1
+    

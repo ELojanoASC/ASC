f87d9663b7c0a093524a64dc3effaf250df96b0b
ELojanoASC <ErickJLojano@gmail.com>
2016-07-15 09:13:05 -0400

    ConditonalsExample

---
+++ ConditionalsExample.py
@@ -0,0 +1,14 @@
+score = 85
+
+
+if score < 60:
+    print("You failed")
+    
+elif score < 80:
+    print("You did alright")
+    
+elif score < 90:
+    print ("You did pretty well")
+    
+else:
+    print("Great Job!")
